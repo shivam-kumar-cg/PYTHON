9ab74791258f9f0a00fe7390ee7764bdb2b17be2
shivam-kumar-cg <shivam.kumar.s.cg@gmail.com>
2026-08-26 11:44:00 +0530

    add 04 arithmetic _operators

---
+++ 04.Arithmetic _operators/04Arithmetic_operators.py
@@ -0,0 +1,160 @@
+#task 1
+a=6
+b=4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+#task 2
+a=3
+b=1.5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+#task 3
+p=physics=86
+c=chemistry=66
+m=maths=85
+print(p+m+c)
+print((p+c+m)/3)
+
+#task 4
+product_price=20
+quantity=4
+print(product_price*quantity)
+
+#task 5
+d=46
+e=65
+print(d%2)
+print(e%2)
+
+#task 6
+a=14
+b=3
+print(a/b)
+print(a//b)
+
+#task 7
+a=-6
+b=-2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+#task 8
+a=10
+b=5
+c=-4
+d=-2
+print(a-b)
+print(a-c)
+print(c-a)
+print(c-d)
+
+#task 9
+print(a//b)
+print(c//a)
+print(a//c)
+print(c//d)
+
+#task 10
+print(a%b)
+print(c%a)
+print(a%c)
+print(c%d)
+
+#task 11
+print(10 + 5 * 2)
+print(20 - 4 / 2)
+print(10 + 20 / 5 * 2)
+print(2 + 3 * 4 ** 2)
+print(100 - 20 // 5)
+
+#task 12
+print(10 + 5 * 2)
+print((10 + 5) * 2)
+
+print(20 - 10 / 2)
+print((20 - 10) / 2)
+
+print(2 + 3 * 4)
+print((2 + 3) * 4)
+
+#task 13
+a=True
+b=False
+print(a+b)
+print(a-b)
+print(a*b)
+print(a**b)
+
+#task 14
+print(True + 5)
+print(False + 5)
+print(True * 10)
+print(False * 10)
+print(True - 5)
+print(False - 5)
+
+#task 15
+a="shivam"
+b="kumar"
+print(a+b)
+
+#task 16
+a="shivam"
+b=3
+print(a*b)
+
+#task 17
+a="shivam"
+b="kumar"
+c=3
+print(a+b)
+print(a*c)
+
+#task 18
+a=3
+b=None
+#print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
+
+#task 19
+a=0
+b=12
+# print(b/a)
+
+#task 20
+a=6
+b=4
+print("addition",a+b)
+print("subtraction",a-b)
+print("multiplication",a*b)
+print("division",a/b)
+print("floor division",a//b)
+print("Modulus:",a%b)
+print("Exponentiation",a**b)
+
+#task 21
+a = 10
+b = -3
+c = 2.5
--- about_python.py
+++
@@ -1,7 +0,0 @@
-print("hello world")
-print("shivam kumar")
-print(10+2)
-print(10/2)
-a=15
-b=24
-print(a+b)
