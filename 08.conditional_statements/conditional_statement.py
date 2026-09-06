965e8e4ba71c274642a55064bdeeabbce3ff27b9
shivam-kumar-cg <shivam.kumar.s.cg@gmail.com>
2026-09-06 22:26:46 +0530

    add this file

---
+++ 08.conditional_statements/conditional_statement.py
@@ -0,0 +1,62 @@
+#1
+number=int(input("enter a number :"))
+if number>10:
+    print("Greater than 10")
+
+#2
+age=int(input("enter your age :"))
+if age>=18:
+    print("adult")
+
+#3
+number=int(input("enter a number :"))
+if number>=0:
+    print("positive")
+
+#4
+marks=int(input("enter your marks :"))
+if marks>=40:
+    print("pass")
+
+
+#5
+number=int(input("enter a number :"))
+if number==0:
+    print("Zero")
+
+
+#6
+number=int(input("enter a number :"))
+if number>0:
+    print("positive")
+else:
+    print("not positive")    
+    
+#7
+age=int(input("enter your age :"))
+if age>=18:
+    print("Adult")
+else:
+    print("minor")
+
+#48
+number=int(input("enter a number :"))
+if number%2==0:
+    print("even")
+else:
+    print("odd")
+
+#9
+marks=int(input("enter your marks :"))
+if marks>=40:
+    print("pass")
+else:
+    print("fail")        
+
+#10
+number1=int(input("enter a number :"))
+number2=int(input("enter another number :"))
+if number1>number2:
+    print("happy")
+else:
+    print("sad")                           
